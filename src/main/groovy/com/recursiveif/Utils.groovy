a52889ad48195bb99c4f9c77fca7003bab60360a
marmaravi <marco.martinez@bq.com>
2018-05-23 22:53:23 +0200

    Update recursiveIf to accept closure parameters

--- src/main/groovy/com/recursiveif/Utils.groovy
+++ src/main/groovy/com/recursiveif/Utils.groovy
@@ -1,5 +1,6 @@
 package com.recursiveif
 
+@SuppressWarnings("GroovyAssignabilityCheck")
 class Utils {
 
     /**
@@ -9,26 +10,38 @@ class Utils {
      * If one of the closures return an object, method execution will finish and
      * that object will be returned
      *
-     * @param conditionList List with closures where the conditions are defined
+     * @param conditions List with closures where the conditions are defined
      *
      * if (n == 1) return 'x'
      * else if (n == 2) return 'y'
      * else return 'z'
      *
-     * List<Closure> conditionList = [
+     * List<Closure> conditions = [
      *     { n == 1 ? 'x' : null },
      *     { n == 2 ? 'y' : null },
      *     { z }
      * ]
+     * List<Closure> paramsConditions = [
+     *     { int n -> n == 1 ? 'x' : null },
+     *     { int n -> n == 2 ? 'y' : null },
+     *     { z }
+     * ]
+     * List<Closure> listParamsConditions = [
+     *     { int n1, int n2 -> n1 == 1 && n2 == 1 ? 'x' : null },
+     *     { int n1, int n2 -> n1 == 2 && n2 == 2 ? 'y' : null },
+     *     { z }
+     * ]
      *
-     * @param conditionList List<Closure> with the conditions
-     * @param index         Default 0, used in the recursive algorithm
-     * @return              Returned object depending on the conditions
+     * @param conditions List<Closure> with the conditions
+     * @param params Object or List with the closures params
+     * @param index Default 0, used in the recursive algorithm
+     * @return Returned object depending on the conditions
      */
-    static recursiveIf(List<Closure> conditionList, int index = 0) {
-        def returnObject = conditionList.get(index++)()
-        if (!returnObject && index < conditionList.size())
-            returnObject = recursiveIf(conditionList, index)
+    static recursiveIf(ArrayList<Closure> conditions, params = null, int index = 0) {
+        def returnObject = conditions.get(index++)(params)
+        if (!returnObject && index < conditions.size()) {
+            returnObject = recursiveIf(conditions, params, index)
+        }
 
         return returnObject
     }
--- src/test/groovy/com/recursiveif/UtilsTest.groovy
+++ src/test/groovy/com/recursiveif/UtilsTest.groovy
@@ -4,14 +4,24 @@ import spock.lang.Specification
 
 class UtilsTest extends Specification {
 
-    List<Closure> conditionList
+    ArrayList<Closure> conditions, paramsConditions, listParamsConditions
     int n
 
     void setup() {
-        conditionList = [
+        conditions = [
                 { n == 1 ? 'I am the if!' : null },
                 { n == 2 ? 'I am the else if!' : null },
                 { 'I am the else!' }
+        ]
+        paramsConditions = [
+                { int param -> param == 1 ? 'I am the if!' : null },
+                { int param -> param == 2 ? 'I am the else if!' : null },
+                { int param -> 'I am the else!' }
+        ]
+        listParamsConditions = [
+                { int param1, int param2 -> param1 == 1 && param2 == 1 ? 'I am the if!' : null },
+                { int param1, int param2 -> param1 == 2 && param2 == 2 ? 'I am the else if!' : null },
+                { int param1, int param2 -> 'I am the else!' }
         ]
     }
 
@@ -20,7 +30,7 @@ class UtilsTest extends Specification {
             n = 1
 
         when:
-            String conditionsResult = Utils.recursiveIf(conditionList)
+            String conditionsResult = Utils.recursiveIf(conditions)
 
         then:
             conditionsResult == 'I am the if!'
@@ -31,7 +41,7 @@ class UtilsTest extends Specification {
             n = 2
 
         when:
-            String conditionsResult = Utils.recursiveIf(conditionList)
+            String conditionsResult = Utils.recursiveIf(conditions)
 
         then:
             conditionsResult == 'I am the else if!'
@@ -42,7 +52,73 @@ class UtilsTest extends Specification {
             n = 1234
 
         when:
-            String conditionsResult = Utils.recursiveIf(conditionList)
+            String conditionsResult = Utils.recursiveIf(conditions)
+
+        then:
+            conditionsResult == 'I am the else!'
+    }
+
+    void "test params recursive if 'if branch'"() {
+        given:
+            int param = 1
+
+        when:
+            String conditionsResult = Utils.recursiveIf(paramsConditions, param)
+
+        then:
+            conditionsResult == 'I am the if!'
+    }
+
+    void "test params recursive if 'else if branch'"() {
+        given:
+            int param = 2
+
+        when:
+            String conditionsResult = Utils.recursiveIf(paramsConditions, param)
+
+        then:
+            conditionsResult == 'I am the else if!'
+    }
+
+    void "test params recursive if 'else branch'"() {
+        given:
+            int param = 1234
+
+        when:
+            String conditionsResult = Utils.recursiveIf(paramsConditions, param)
+
+        then:
+            conditionsResult == 'I am the else!'
+    }
+
+    void "test list params recursive if 'if branch'"() {
+        given:
+            ArrayList<Integer> params = [1, 1]
+
+        when:
+            String conditionsResult = Utils.recursiveIf(listParamsConditions, params)
+
+        then:
+            conditionsResult == 'I am the if!'
+    }
+
+    void "test list params recursive if 'else if branch'"() {
+        given:
+            ArrayList<Integer> params = [2, 2]
+
+        when:
+            String conditionsResult = Utils.recursiveIf(listParamsConditions, params)
+
+        then:
+            conditionsResult == 'I am the else if!'
+    }
+
+    void "test list params recursive if 'else branch'"() {
+        given:
+            ArrayList<Integer> params = [3, 4]
+
+        when:
+            String conditionsResult = Utils.recursiveIf(listParamsConditions, params)
 
         then:
             conditionsResult == 'I am the else!'
